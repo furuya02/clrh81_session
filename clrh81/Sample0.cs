9c9726ee8914e99764c018f33dd246252c19970f
furuya02 <sin@sapporoworks.ne.jp>
2013-04-10 04:39:11 +0900

    2013.04.10.0440

---
+++ clrh81/Sample0.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace clrh81 {
+    class Sample0 {
+        
+        private int[] _list = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public void Sample_000(){
+            var ar = new List<int>();
+            foreach (var i in _list){
+                if (i%2 == 0 && i < 5)
+                    ar.Add(i);
+            }
+        }
+
+        public void Sample_001() {
+            var ar = new List<int>();
+            foreach (var i in _list) {
+                if (i % 2 == 0) {
+                    foreach (var n in _list){
+                        if(n==i){
+                            ar.Add(i);
+                        }
+                    
+                    }
+                }
+            }
+        }
+
+
+        class OneRR{
+            public int DnsType { get; set; }
+            public bool IsEffective{get; set; }
+            public string Name { get; set; }
+            public string Data { get; set; }
+        }
+
+        public string Sample_002() {
+            var log = "";
+            var db = new List<OneRR>();
+            foreach (var oneRr in db) {
+                if (oneRr.DnsType == 1) {
+                    if (!oneRr.IsEffective)
+                        continue;
+                    if (oneRr.Name.ToUpper() == "ABC") {
+                        var find = false;
+                        foreach (var o in db) {
+                            if (o.Data == oneRr.Data) {
+                                find = true;
+                                break;
+                            }
+                        }
+                        if (!find) {
+                            log="Success data search.";
+
+                        }
+                    }
+                }
+            }
+            return log;
+        }
+        
+    }
+}
---
+++ clrh81/Sample1.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace clrh81 {
+    class Sample1{
+        //******************************************************************
+        // 以下のサンプルは、下のリストを対象にしています
+        //******************************************************************
+        private readonly int[] _list = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        
+        //******************************************************************
+        // 【判定】AnyとAllはフラグの真偽で判断される
+        //******************************************************************
+        public void 条件を満たす要素が含まれているか_Any() {
+            var flg=false;
+            foreach (var i in _list){
+                if (i < 5){
+                    flg = true;
+                    break;
+                }
+            }
+        }
+        
+        public void すべての要素が条件を満たしてるか_All() {
+            var flg = true;
+            foreach (var i in _list) {
+                if (i < 5) {
+                    flg=false;
+                    break;
+                }
+            }
+        }
+
+
+        //******************************************************************
+        //【集計】は結構うまく動作している
+        //******************************************************************
+        public void　要素のカウント_count() {
+            var count = 0;
+            foreach (var i in _list){
+                count++;
+            }
+        }
+
+        public void 要素の合計_Sum() {
+            var sum = 0;
+            foreach (var i in _list) {
+                sum+=i;
+            }
+        }
+        //微妙
+        public void 最大値_Max() {
+            var max = 0;
+            foreach (var i in _list) {
+                if (max < i) {
+                    max = i;
+                }
+            }
+
+            //これでいいんじゃ無いだろうか・・・
+            //max = _list.Max();
+        }
+
+
+
+        //******************************************************************
+        // 【複数の要素取得】は、微妙にWhereや～Whileに変換される
+        //******************************************************************
+        public void 条件を満たす要素の抽出() {
+            var res = new List<int>();
+            foreach (var i in _list) {
+                if (i > 5) {
+                    res.Add(i);
+                }
+            }
+        }
+        //微妙
+        public void 先頭から３つをスキップ() {
+            var res = new List<int>();
+            
+            var count = 0;
+            foreach (var i in _list){
+                if (count++ < 3){
+                    continue;
+                }
+                res.Add(i);
+            }
+            
+            //これでいいんじゃ無いだろうか・・・
+            //res = _list.Skip(3).ToList();
+        }
+        //微妙
+        public void 先頭から３つだけを取得() {
+            var res = new List<int>();
+
+            var count = 0;
+            foreach (var i in _list) {
+                if (count++ < 3) {
+                    break;
+                }
+                res.Add(i);
+            }
+
+            //これでいいんじゃ無いだろうか・・・
+            //res = _list.Take(3).ToList();
+        }
+
+
+
+    }
+}
