7b900333bb18ea95220776c37bfbf8e603eba82c
furuya02 <sin@sapporoworks.ne.jp>
2013-04-12 05:09:10 +0900

    2013.04.12.0510

--- clrh81/Sample0.cs
+++ clrh81/Sample0.cs
@@ -5,6 +5,9 @@ namespace clrh81 {
         
         private int[] _list = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+        //******************************************************
+        // なんなくWhereで解決される
+        //******************************************************
         public void Sample_000(){
             var ar = new List<int>();
             foreach (var i in _list){
@@ -12,20 +15,22 @@ namespace clrh81 {
                     ar.Add(i);
             }
         }
-
+        //******************************************************
+        // 抽出されたオブジェクトに操作が入ると
+        // from ～ in  ～  where ～ select で解決される
+        //******************************************************
         public void Sample_001() {
-            var ar = new List<int>();
+            var res = new List<int>();
             foreach (var i in _list) {
                 if (i % 2 == 0) {
-                    foreach (var n in _list){
-                        if(n==i){
-                            ar.Add(i);
-                        }
-                    
-                    }
+                    res.Add(Func(i));
                 }
             }
         }
+        private int Func(int i) {
+            return i;
+        }
+
 
 
         class OneRR{
@@ -35,6 +40,9 @@ namespace clrh81 {
             public string Data { get; set; }
         }
 
+        //******************************************************
+        // 多重の条件処理も力ずくで？
+        //******************************************************
         public string Sample_002() {
             var log = "";
             var db = new List<OneRR>();
---
+++ clrh81/Sample2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace clrh81 {
+//    class Sample2{
+//
+//
+//
+//        List<int> numList = new List<int>();
+//
+//        public IEnumerable<int> GetEvenNums() {
+//            //lock (numList) {
+//                return numList.Where(i => i % 2 == 0);
+//            //}
+//        }
+//
+//        public void AddNum(int num) {
+//            //lock (numList) {
+//                numList.Add(num);
+//            //}
+//        }
+//
+//    }
+//
+    class DbClass{
+        List<int> _list = new List<int>();
+
+        public IEnumerable<int> GetList() {
+            //lock (numList) {
+            return _list.Where(i => i % 2 == 0);
+            //}
+        }
+
+        public void Add(int i) {
+            //lock (numList) {
+            _list.Add(i);
+            //}
+        }
+    }
+
+    class Sample2{
+        
+
+        
+    }
+}
